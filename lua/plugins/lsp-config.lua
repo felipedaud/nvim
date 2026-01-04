1adbaa80b6f36a7ae20c1cf427db1f88b210a0f1
Felipe <felipe@DESKTOP-0I5C734.localdomain>
2026-01-04 17:01:39 -0300

    wip

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -130,6 +130,8 @@ return {
     -- - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
     -- - settings (table): Override the default settings passed when initializing the server.
     local servers = {
+        omnisharp = {},
+        netcoredbg = {},
       ts_ls = {},
       ruff = {},
       pylsp = {
